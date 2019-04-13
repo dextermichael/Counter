ca987c78dbb3d585358e2a8ae4140489dc98b700
Dexter Hennington <dextermichael.h@gmail.com>
2019-04-13 05:28:38 -0400

    Commeneted out basics of redux

--- App.js
+++ App.js
@@ -1,36 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-
+import CounterApp from "src/CounterApp";
 export default class App extends React.Component {
-  state = {
-    counter: 0
-  };
-  increaseCounter = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-  decreaseCounter = () => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
+  /**
+   * Store - holds our state - THERE IS ONLY ONE STATE
+   * Action - State can mofified using actions - SIMPLE OBJECTS
+   * Dispactcher - Action needs to be sent by someone - known as dispatching an action
+   * Reducer - recieves the action and modifes the state to give us a new state
+   * -pure funtion
+   * -only mandotory/arugment is the 'type;
+   * Subscriber -listens for state change to update the ui (using connect)
+   */
+
   render() {
-    return (
-      <View style={styles.container}>
-        <View
-          style={{
-            flexDirection: "row",
-            width: 200,
-            justifyContent: "space-around"
-          }}
-        >
-          <TouchableOpacity onPress={() => this.increaseCounter()}>
-            <Text style={{ fontSize: 20 }}>Increase</Text>
-          </TouchableOpacity>
-          <Text style={{ fontSize: 20 }}>{this.state.counter}</Text>
-          <TouchableOpacity onPress={() => this.decreaseCounter()}>
-            <Text style={{ fontSize: 20 }}>Decrease</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
+    return <CounterApp />;
   }
 }
 
